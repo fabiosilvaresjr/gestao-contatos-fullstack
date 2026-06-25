f3051729848b7020a6bd996101c50994f2e6985d
fabio.junior <fabio.junior@placeti.com.br>
2026-06-25 15:03:17 -0300

    fix: corrigir tipagem do id do usuario para suportar uuid

--- Exercicio_SpringBoot/src/main/java/com/example/exercicio_springboot/exception/GlobalExceptionHandler.java
+++ Exercicio_SpringBoot/src/main/java/com/example/exercicio_springboot/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.example.exercicio_springboot.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,15 +20,27 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
+    // Erro 404
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
 
-    // DTO interno apenas para formatar a resposta do erro
+    //  Erro 403
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<MensagemErro> tratarErro403(AccessDeniedException ex) {
+        // Retorna status 403 FORBIDDEN com uma mensagem limpa e profissional
+        var erro = new MensagemErro("Acesso Negado", "Você não tem permissão para realizar esta ação.");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
+    }
+
+    // DTO interno para os erros do @Valid
     private record DadosErroValidacao(String campo, String mensagem) {
         public DadosErroValidacao(FieldError erro) {
             this(erro.getField(), erro.getDefaultMessage());
         }
     }
+
+    // DTO interno para mensagens personalizadas
+    private record MensagemErro(String erro, String mensagem) {}
 }
