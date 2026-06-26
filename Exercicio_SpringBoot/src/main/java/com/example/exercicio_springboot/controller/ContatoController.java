8429ce0c0c6d4197373920e45f80755b992ff200
fabio.junior <fabio.junior@placeti.com.br>
2026-06-26 17:50:01 -0300

    config: habilitar CORS para integracao com Angular
    - Adicionado .cors(Customizer.withDefaults()) no SecurityConfigurations.
    - Inserida anotação @CrossOrigin(origins = "http://localhost:4200") nos Controllers (Auth, Contato, Grupo e Etiqueta).
    - API preparada para receber requisições do front-end sem bloqueios de segurança do navegador.

--- Exercicio_SpringBoot/src/main/java/com/example/exercicio_springboot/controller/ContatoController.java
+++ Exercicio_SpringBoot/src/main/java/com/example/exercicio_springboot/controller/ContatoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contatos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ContatoController {
 
     private final ContatoService contatoService;
--- Exercicio_SpringBoot/src/main/java/com/example/exercicio_springboot/controller/EtiquetaController.java
+++ Exercicio_SpringBoot/src/main/java/com/example/exercicio_springboot/controller/EtiquetaController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/etiquetas")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EtiquetaController {
 
     private final EtiquetaService etiquetaService;
