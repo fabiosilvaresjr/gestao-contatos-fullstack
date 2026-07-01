de68fa8726c7bb550de4754b583999095550eccc
fabio.junior <fabio.junior@placeti.com.br>
2026-07-01 11:39:08 -0300

    fix: configura ambiente de testes - Atualiza java.version no pom.xml - Remove scope test temporariamente para sincronia da IDE - Sucesso no ExercicioSpringBootApplicationTests.contextLoads()

---
+++ Exercicio_SpringBoot/src/test/java/com/example/exercicio_springboot/ExercicioSpringBootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.exercicio_springboot;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // iniciar o projeto
+    }
+}
+
